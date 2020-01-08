063a9c1d5292d518ab21a6e57070da52ac98d708
hiba02 <kangyoonsoon@hotmail.com>
2020-01-08 21:28:55 +0000

    1st letterPositions.js

---
+++ letterPositions.js
@@ -0,0 +1,38 @@
+// FUNCTION IMPLEMENTATION
+const eqArrays = function(actualArr, expectedArr) {
+  if (actualArr.length !== expectedArr.length) {
+    return "The length of two arrays is not equal.";
+  } else {
+    for (let i = 0; i < actualArr.length; i++) {
+      if (actualArr[i] !== expectedArr[i]) {
+        return false;
+      }
+    }
+    return true;
+  }
+};
+
+const assertArraysEqual = function(actualArr, expectedArr) {
+  if (eqArrays(actualArr,expectedArr)) {
+    console.log("The arrays are same.");
+  } else {
+    console.log("The arrays are different.");
+  }
+};
+
+const letterPositions = function(sentence) {
+  const results = {};
+  for (let i = 0; i < sentence.length; i++) {
+    const arr = [];   
+    if (!results[sentence[i]]) {
+      arr.push(i);
+      results[sentence[i]] = arr;
+    } else {
+      results[sentence[i]].push(i);
+    } 
+  }
+  return results;
+};
+
+console.log(letterPositions("hello"));
+assertArraysEqual(letterPositions("hello").e, [1]);
