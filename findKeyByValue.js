79a9594746060e9e234d3539740b9ef4fdc9cc2b
hiba02 <kangyoonsoon@hotmail.com>
2020-01-08 21:51:15 +0000

    1st findKeyByValue

---
+++ findKeyByValue.js
@@ -0,0 +1,44 @@
+const assertEqual = function(actual, expected) {
+  if (typeof actual === 'number' && typeof expected === 'number') {
+    if (actual === expected) {
+      console.log(`emojis Assertion Passed: ${actual} === ${expected}`);
+    } else {
+      console.log(`emojis Assertion Failed: ${actual} !== ${expected}`);
+    }
+  } else {
+    if (actual === expected) {
+      console.log(`emojis Assertion Passed: "${actual}" === "${expected}"`);
+    } else {
+      console.log(`emojis Assertion Failed: "${actual}" !== "${expected}"`);
+    }
+  }
+};
+/*
+  for (const item of allItems) {
+    if (results[item]) {
+      results[item] += 1;
+    } else {
+      results[item] = 1;
+    }
+  }
+*/
+const findKeyByValue = function(isObj, isValue) {
+  for (let title in isObj) {
+    if (isObj[title] === isValue) {
+      return title;
+    }
+  }
+};
+
+// TEST
+const bestTVShowsByGenre = { 
+  sci_fi: "The Expanse",
+  comedy: "Brooklyn Nine-Nine",
+  drama:  "The Wire"
+};
+
+console.log(findKeyByValue(bestTVShowsByGenre, "The Wire"));
+console.log(findKeyByValue(bestTVShowsByGenre, "balbla"));
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
+assertEqual(findKeyByValue(bestTVShowsByGenre, "Brooklyn Nine-Nine"), "comedy");
