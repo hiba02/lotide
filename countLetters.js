15c1832b0f9ca69b26e3346a78b1ae04625f3253
hiba02 <kangyoonsoon@hotmail.com>
2020-01-08 20:26:10 +0000

    1st countLetters.js

---
+++ countLetters.js
@@ -0,0 +1,41 @@
+const assertEqual = function(actual, expected) {
+  if (typeof actual === 'number' && typeof expected === 'number') {
+    if (actual === expected) {
+      console.log(`emojis Assertion Passed: ${actual} === ${expected}`);
+    } else {
+      console.log(`emojis Assertion Failed: ${actual} !== ${expected}`);
+    }
+  } else {
+    if (actual === expected) {
+      console.log(`emojis Assertion Passed: "${actual}" === "${expected}"`);
+    } else {
+      console.log(`emojis Assertion Failed: "${actual}" !== "${expected}"`);
+    }
+  }
+};
+/*
+  for (const item of allItems) {
+    if (results[item]) {
+      results[item] += 1;
+    } else {
+      results[item] = 1;
+    }
+  }
+*/
+const countLetters = function(str) {
+  const results = {};
+  for (let letter of str) {
+    if (letter !== ' ') {
+      if (results[letter]) {
+        results[letter] += 1;
+      } else {
+        results[letter] = 1;
+      }
+    }
+  }
+  return results;
+};
+
+//TEST
+console.log(countLetters("lighthouse in the house")); 
+assertEqual(countLetters("lighthouse in the house"), countLetters("lighthouse in the house")); 
