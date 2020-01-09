a9061183903dcbc445779535ae24d6f2dc69775f
hiba02 <kangyoonsoon@hotmail.com>
2020-01-09 22:20:11 +0000

    1st findKey.js

---
+++ findKey.js
@@ -0,0 +1,56 @@
+// FUNCTION IMPLEMENTATION
+const assertEqual = function(actual, expected) {
+  if (typeof actual === 'number' && typeof expected === 'number') {
+    if (actual === expected) {
+      console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+    } else {
+      console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+    }
+  } else {
+    if (actual === expected) {
+      console.log(`✅✅✅  Assertion Passed: "${actual}" === "${expected}"`);
+    } else {
+      console.log(`🛑🛑🛑 Assertion Failed: "${actual}" !== "${expected}"`);
+    }
+  }
+};
+
+
+/*
+Implement the function findKey which takes in an object and a callback. It should scan the object and return the first key for which the callback returns a truthy value. If no key is found, then it should return undefined.
+findKey({
+  "Blue Hill": { stars: 1 },
+  "Akaleri":   { stars: 3 },
+  "noma":      { stars: 2 },
+  "elBulli":   { stars: 3 },
+  "Ora":       { stars: 2 },
+  "Akelarre":  { stars: 3 }
+}, x => x.stars === 2) // => "noma"
+*/
+/*
+const map = function(array, callback) {
+  const results = [];
+  for (let item of array) {
+    results.push(callback(item));
+  }
+  return results;
+};
+*/
+const findKey = function(anObject, callback) {
+  for (oneObj in anObject) {
+    if (callback(anObject[oneObj])) {
+      return oneObj;
+    }
+  }
+  return;
+};
+const result = findKey({
+  "Blue Hill": { stars: 1 },
+  "Akaleri":   { stars: 3 },
+  "noma":      { stars: 2 },
+  "elBulli":   { stars: 3 },
+  "Ora":       { stars: 2 },
+  "Akelarre":  { stars: 3 }
+}, x => x.stars === 2)
+
+console.log(result);
