2aee0212e24e500a2236a8b7ea3c0f9eb2385ef4
hiba02 <kangyoonsoon@hotmail.com>
2020-01-13 19:54:16 +0000

    changed middle.js

--- middle.js
+++ middle.js
@@ -36,8 +36,8 @@ const middle = function(array) {
 };
 
 //TEST CODE
-assertArraysEqual(middle([1, 2, 3]), middle([1, 2, 3, 4, 5]));
+assertArraysEqual(middle([]), middle([1, 2]));
 assertArraysEqual(middle([1, 2, 3]), middle([1, 2, 3]));
 assertArraysEqual(middle([1, 2, 3, 4]), middle([1, 2, 3, 4]));
-
-
+console.log(middle([]));
+console.log(middle([1, 2]));
