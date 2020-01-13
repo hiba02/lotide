e91aada57b48e125b020519bd9c209df732fa567
hiba02 <kangyoonsoon@hotmail.com>
2020-01-13 15:00:39 +0000

    modified assertEqual.js head.js

--- assertEqual.js
+++ assertEqual.js
@@ -15,7 +15,4 @@ const assertEqual = function(actual, expected) {
   }
 };
 
-// TEST CODE
-assertEqual("Lighthouse Labs", "Bootcamp");
-assertEqual(1, 1);
-assertEqual(1, "apple");
+module.exports = assertEqual;
--- head.js
+++ head.js
@@ -1,22 +1,6 @@
-const assertEqual = function(actual, expected) {
-  if (typeof actual === 'number' && typeof expected === 'number') {
-    if (actual === expected) {
-      console.log(`emojis Assertion Passed: ${actual} === ${expected}`);
-    } else {
-      console.log(`emojis Assertion Failed: ${actual} !== ${expected}`);
-    }
-  } else {
-    if (actual === expected) {
-      console.log(`emojis Assertion Passed: "${actual}" === "${expected}"`);
-    } else {
-      console.log(`emojis Assertion Failed: "${actual}" !== "${expected}"`);
-    }
-  }
-};
-
+const assertEqual = require('./assertEqual');
 const head = function(arr) {
   return arr[0];
 }
 
-assertEqual(head([5,6,7]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+module.exports = head;
