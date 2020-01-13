41e56469374f769c65c92122095d8cf87b220339
hiba02 <kangyoonsoon@hotmail.com>
2020-01-13 18:20:08 +0000

    modified eqArrays.js tail.js for module

--- eqArrays.js
+++ eqArrays.js
@@ -32,8 +32,10 @@ const eqArrays = function(actualArr, expectedArr) {
 // assertEqual("Lighthouse Labs", "Bootcamp");
 // assertEqual(1, 1);
 // assertEqual(1, "apple");
-console.log(eqArrays([1, 2, 3], [1, 2, 3])); // => true
-console.log(eqArrays([1, 2, 3], [3, 2, 1]));
-console.log(eqArrays(["1", "2", "3"], ["1", "2", "3"]));
-console.log(eqArrays(["1", "2", "3"], ["1", "2", 3]));
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
+// console.log(eqArrays([1, 2, 3], [1, 2, 3])); // => true
+// console.log(eqArrays([1, 2, 3], [3, 2, 1]));
+// console.log(eqArrays(["1", "2", "3"], ["1", "2", "3"]));
+// console.log(eqArrays(["1", "2", "3"], ["1", "2", 3]));
+// assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
+
+module.exports = eqArrays;
--- tail.js
+++ tail.js
@@ -1,18 +1,4 @@
-const assertEqual = function(actual, expected) {
-  if (typeof actual === 'number' && typeof expected === 'number') {
-    if (actual === expected) {
-      console.log(`emojis Assertion Passed: ${actual} === ${expected}`);
-    } else {
-      console.log(`emojis Assertion Failed: ${actual} !== ${expected}`);
-    }
-  } else {
-    if (actual === expected) {
-      console.log(`emojis Assertion Passed: "${actual}" === "${expected}"`);
-    } else {
-      console.log(`emojis Assertion Failed: "${actual}" !== "${expected}"`);
-    }
-  }
-};
+//const assertEqual = require('./assertEqual');
 
 const tail = function(arr) {
   const newArr = [];
@@ -23,10 +9,7 @@ const tail = function(arr) {
   }
   return newArr;
 };
+ 
 
-const result = tail(["Hello", "Lighthouse", "Labs"]);
-assertEqual(result.length, 2); // ensure we get back two elements
-assertEqual(result[0], "Lighthouse"); // ensure first element is "Lighthouse"
-assertEqual(result[1], "Labs"); // ensure second element is "Labs"
-console.log(tail([1]));
-console.log(tail([]));
+
+module.exports = tail;
