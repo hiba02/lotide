d8bf7b84fb6921b52073da8728223de63c416669
hiba02 <kangyoonsoon@hotmail.com>
2020-01-13 19:53:28 +0000

    1st tailTest mocha

---
+++ test/tailTest.js
@@ -0,0 +1,26 @@
+const assert = require('chai').assert;
+const tail = require('../tail'); 
+
+
+describe ("Test tail function", function () {
+  it ("returns two elements for ['Hello', 'Lighthouse', 'Labs']", function() {
+    const result = tail(["Hello", "Lighthouse", "Labs"]);
+    assert.strictEqual(result.length, 2);
+  });
+
+  it ("returns the second element in the array", function() {
+    const result = tail(["Hello", "Lighthouse", "Labs"]);
+    assert.strictEqual(result[1], "Labs");
+  });
+
+  it ("returns the second element in the array", function() {
+    const result = tail(["Hello", "Lighthouse", "Labs"]);
+    assert.deepEqual(result, ["Lighthouse", "Labs"]);
+  });
+
+})
+
+
+// const result = tail(["Hello", "Lighthouse", "Labs"]);
+// assertEqual(result[0], "Lighthouse"); // ensure first element is "Lighthouse"
+// assertEqual(result[1], "Labs"); // ensure second element is "Labs"
