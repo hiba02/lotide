cdee8476d3d6c53a3d7541d827643b1e310b07fc
hiba02 <kangyoonsoon@hotmail.com>
2020-01-13 18:19:10 +0000

    change assertArrayEqualTest.js assertArraysEqual.js for module export

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,31 +1,31 @@
 // FUNCTION IMPLEMENTATION
-const eqArrays = function(actualArr, expectedArr) {
-  if (actualArr.length !== expectedArr.length) {
-    return "The length of two arrays is not equal.";
-  } else {
-    for (let i = 0; i < actualArr.length; i++) {
-      if (actualArr[i] !== expectedArr[i]) {
-        return false;
-      }
-    }
-    return true;
-  }
-};
-
+// const eqArrays = function(actualArr, expectedArr) {
+//   if (actualArr.length !== expectedArr.length) {
+//     return "The length of two arrays is not equal.";
+//   } else {
+//     for (let i = 0; i < actualArr.length; i++) {
+//       if (actualArr[i] !== expectedArr[i]) {
+//         return false;
+//       }
+//     }
+//     return true;
+//   }
+// };
+const eqArrays = require('./eqArrays');
 
 const assertArraysEqual = function(actualArr, expectedArr) {
   if (eqArrays(actualArr,expectedArr)) {
-      console.log("The arrays are same.");
+    console.log(`🔴🔴🔴: Assertion Passed: ${actualArr} === ${expectedArr}`);
   } else {
-    console.log("The arrays are different.");
+    console.log(`💔💔💔: Assertion Failed: ${actualArr} !== ${expectedArr}`);
   }
 };
 
-
+module.exports = assertArraysEqual;
 
 // TEST CODE
-assertArraysEqual([1, 2, 3], [1, 2, 3]);
-assertArraysEqual([1, 2, 3], [1, 2, 4]);
+// assertArraysEqual([1, 2, 3], [1, 2, 3]);
+// assertArraysEqual([1, 2, 3], [1, 2, 4]);
 
 
 
